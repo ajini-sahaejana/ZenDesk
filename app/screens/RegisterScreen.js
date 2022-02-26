b825085231c5969c9149dd13d3cea25e6a71fda6
Ajini Sahejana <ajisahejana@gmail.com>
2022-02-26 06:44:10 +0530

    Update RegisterScreen.js
    Using Custom Code Snippets

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function RegisterScreen(props) {
+  return <View style={styles.container}></View>;
+}
+
+export default RegisterScreen;
